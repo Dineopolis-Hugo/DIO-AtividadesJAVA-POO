37408e9e09b18f292e9dfb3ff38fca5eb958fa0e
Dineopolis Hugo <hugoamorimfilho@gmail.com>
2026-06-10 15:21:06 -0300

    Add Package e atividades Encapsulamento

---
+++ src/Encapsulamento/computadorPedrinho.java
@@ -0,0 +1,13 @@
+package Encapsulamento;
+
+public class computadorPedrinho {
+    public static void main(String[] args) {
+        //Abrindo o MSN messenger();
+        MSNMesseger msn = new MSNMesseger();
+
+
+        msn.enviarMensagem();
+
+        msn.receberMensagem();
+    }
+}
